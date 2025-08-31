2d5a1b7560938dac826f51889588f5f8d09a2d6b
CutiRomero02 <124090467@link.cuhk.edu.cn>
2025-08-31 11:05:23 +0800

    start training

--- flappy_bird/agent.py
+++ flappy_bird/agent.py
@@ -11,6 +11,12 @@ import yaml
 
 import os
 
+import numpy as np
+import argparse
+
+import matplotlib
+import matplotlib.pyplot as plt
+
 DATE_FORMAT = "%Y%m%d-%H%M%S"
 
 RUNS_DIR = "runs"
@@ -46,6 +52,16 @@ class Agent:
         self.GRAPH_FILE = os.path.join(RUNS_DIR, f'{self.hyperparameter_set}.png')
 
     def run(self, is_training=True, render=False):
+
+        if is_training:
+            start_time = datetime.now()
+            last_graph_update_time = start_time 
+
+            log_message = f"{start_time.strftime(DATE_FORMAT)} Starting training with hyperparameter set '{self.hyperparameter_set}'"
+            print(log_message)
+            with open(self.LOG_FILE, 'a') as file:
+                file.write(log_message + '\n')
+
         # env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=True)
         env = gymnasium.make("CartPole-v1", render_mode="human" if render else None)
 
@@ -78,7 +94,7 @@ class Agent:
             terminated = False
             episode_reward = 0.0
 
-            while not terminated:
+            while not terminated and episode_reward < self.stop_on_reward:
                 # Next action:
                 # (feed the observation to your agent here)
                 if is_training and random.random() < epsilon:    
@@ -114,19 +130,46 @@ class Agent:
                     torch.save(policy_dqn.state_dict(), self.MODEL_FILE)
                     best_reward = episode_reward
 
+                current_time = datetime.now()
+                if current_time - last_graph_update_time > datetime.timedelta(seconds=10) :
+                    self.save_graph(rewards_per_episode, epsilon_history)
+                    last_graph_update_time = current_time
 
 
-            epsilon = max(self.epsilon_min, epsilon * self.epsilon_decay)
-            epsilon_history.append(epsilon)
 
-            if len(memory) > self.mini_batch_size:
-                mini_batch = memory.sample(self.mini_batch_size)
-                self.optimize(mini_batch, policy_dqn, target_dqn)
+                
 
-                if step_count > self.network_sync_rate:
-                    target_dqn.load_state_dict(policy_dqn.state_dict())
-                    step_count = 0
+                if len(memory) > self.mini_batch_size:
+                    mini_batch = memory.sample(self.mini_batch_size)
+                    self.optimize(mini_batch, policy_dqn, target_dqn)
 
+                    epsilon = max(self.epsilon_min, epsilon * self.epsilon_decay)
+                    epsilon_history.append(epsilon)
+
+                    if step_count > self.network_sync_rate:
+                        target_dqn.load_state_dict(policy_dqn.state_dict())
+                        step_count = 0
+
+
+    def save_graph(self, rewards_per_episode, epsilon_history):
+        fig = plt.figure(1)
+
+        mean_rewards = np.zeros(len(rewards_per_episode))
+        for x in range(len(mean_rewards)):
+            mean_rewards[x] = np.mean(rewards_per_episode[max(0, x-99):(x+1)])
+        plt.subplot(121) 
+        plt.ylabel('Mean Rewards')
+        plt.plot(mean_rewards)
+
+        plt.subplot(122) 
+        plt.ylabel('Epsilon Decay')
+        plt.plot(epsilon_history)
+
+        plt.subplots_adjust(wspace=1.0, hspace=1.0)
+
+        # Save plots
+        fig.savefig(self.GRAPH_FILE)
+        plt.close(fig)
 
     def optimize(self, mini_batch, policy_dqn, target_dqn):
         '''
@@ -168,5 +211,14 @@ class Agent:
 
 
 if __name__ == "__main__":
-    agent = Agent("cartpole1")
-    agent.run(is_training=True, render=True)
+    parser = argparse.ArgumentParser(description='Train or test model.')
+    parser.add_argument('hyperparameters', help='')
+    parser.add_argument('--train', help='Training mode', action='store_true')
+    args = parser.parse_args()
+
+    dql = Agent(hyperparameter_set=args.hyperparameters)
+
+    if args.train:
+        dql.run(is_training=True)
+    else:
+        dql.run(is_training=False, render=True)
